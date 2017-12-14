044eb0c97d4b8b98bd21ea9a6c0cbfa4b7be380b
Jonathan Riezman <riezman@gmail.com>
2017-12-13 19:11:57 -0600

    added category, categoryDAO, categoryController, and cateogory/index.html

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,22 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping("")
+    public String index(Model model){
+        model.addAttribute("title", "Category Index");
+        model.addAttribute("categories", categoryDao.findAll());
+
+        return "category/index";
+    }
+}
